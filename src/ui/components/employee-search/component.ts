bc6dc1c576020d8e5b0427cf93313d698596ff50
Lee Richmond <lrichmond1@bloomberg.net>
2017-04-24 14:35:45 -0400

    Add basic filtering

--- src/ui/components/employee-search/component.ts
+++ src/ui/components/employee-search/component.ts
@@ -2,8 +2,14 @@ import { tracked } from '@glimmer/component';
 import BaseComponent from '../base-component';
 import { Employee, Position, Department } from '../models';
 
+class Query {
+  @tracked firstNamePrefix;
+  @tracked lastNamePrefix;
+}
+
 export default class EmployeeSearch extends BaseComponent {
   @tracked employees;
+  @tracked query = new Query();
 
   constructor(options) {
     super(options);
@@ -16,6 +22,17 @@ export default class EmployeeSearch extends BaseComponent {
     let scope = Employee
       .includes({ current_position: 'department' })
       .per(5);
+
+    if (this.query.firstNamePrefix) {
+      scope = scope.where({
+        first_name_prefix: this.query.firstNamePrefix
+      });
+    }
+    if (this.query.lastNamePrefix) {
+      scope = scope.where({
+        last_name_prefix: this.query.lastNamePrefix
+      });
+    }
 
     scope.all().then((response) => {
       this.employees = response.data;
